82432f8e6b7d25763b417064657984aa161ed116
lenanasirov <lenanasirov@gmail.com>
2026-07-20 18:49:52 +0300

    Setup React Router and Redux architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+
 
 function App() {
   return (
-    <h1 className="text-5xl font-bold">
-      Echo
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route 
+          path="/login" 
+          element={<Login />} 
+        />
+
+        <Route 
+          path="/register" 
+          element={<Register />} 
+        />
+
+        <Route 
+          path="/profile" 
+          element={<Profile />} 
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
